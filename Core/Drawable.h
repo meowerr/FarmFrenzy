055d884d3440d0af5376248f71b4658294c133ce
meowerrr <Abdullah_alshazly@hotmail.com>
2026-04-06 17:35:30 +0200

    Initial commit

---
+++ Core/Drawable.h
@@ -0,0 +1,23 @@
+#pragma once
+#include "../CMUgraphicsLib/CMUgraphics.h"
+
+class Game;     //forward declaration == will be described later 
+
+struct point //always public 
+{
+	int x, y;
+};
+
+
+class Drawable
+{
+protected: //means only this class and subclasses can access the info 
+	point RefPoint;		//Each game object must have a reference point
+	int width, height;
+	Game* pGame;        //pGame now knows where Game is in the memory
+
+public:
+	Drawable(Game* r_pGame, point r_point, int r_width, int r_height); //constructor
+	virtual void draw() const = 0; //?
+};
+
